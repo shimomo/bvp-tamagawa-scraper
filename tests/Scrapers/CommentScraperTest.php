9acc1d729408aaca8929f11a93be2bfc667cc5dd
shimomo <developer@shimomo.net>
2025-04-15 07:37:49 +0900

    test: Rename `testScrapeWithRaceCode1AndDate20240108()` to `testThrowsExceptionWhenMethodIsNotImplemented()` in `CommentScraperTest`

--- tests/Scrapers/CommentScraperTest.php
+++ tests/Scrapers/CommentScraperTest.php
@@ -28,7 +28,7 @@ final class CommentScraperTest extends TestCase
     /**
      * @return void
      */
-    public function testScrapeWithRaceCode1AndDate20240108(): void
+    public function testThrowsExceptionWhenMethodIsNotImplemented(): void
     {
         $this->expectException(\BadMethodCallException::class);
         $this->expectExceptionMessage(
